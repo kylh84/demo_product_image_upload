1d8c74645c87c9cfa3c0751306a409d8e913b2cb
Kỳ Lê <kylh84@gmail.com>
2025-03-05 13:36:17 +0700

    add application.properties

--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -1,11 +1,20 @@
 package com.example.ecommerce;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceApplication {
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+        System.setProperty("JPA_HIBERNATE_DDL_AUTO", dotenv.get("JPA_HIBERNATE_DDL_AUTO"));
+        System.setProperty("JPA_HIBERNATE_DIALECT", dotenv.get("JPA_HIBERNATE_DIALECT"));
+
         SpringApplication.run(EcommerceApplication.class, args);
     }
 }
